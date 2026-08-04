47813f1def66b0c76234f9b1d43e834c0ecb2330
Bruno Hart <24307321+liu6381810@users.noreply.github.com>
2026-08-04 21:18:57 +0800

    Add QuartzHill Points (QHP) ERC-20 contract draft for Quartz Hill handoff

---
+++ contracts/QuartzHillPoints.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.7.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract QuartzHillPoints is ERC20, ERC20Permit {
+    constructor() ERC20("QuartzHill Points", "QHP") ERC20Permit("QuartzHill Points") {}
+}
